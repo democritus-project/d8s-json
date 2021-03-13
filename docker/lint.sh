527e68c72ff3dd73daef7e10ee5f8b60acfe0476
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-13 09:44:45 -0500

    Changing name to d8s (see https://github.com/democritus-project/roadmap/issues/72)

--- docker/lint.sh
+++ docker/lint.sh
@@ -4,17 +4,17 @@ set -euxo pipefail
 
 echo "Running linters and formatters..."
 
-isort democritus_json/ tests/
+isort d8s_json/ tests/
 
-black democritus_json/ tests/
+black d8s_json/ tests/
 
-mypy democritus_json/ tests/
+mypy d8s_json/ tests/
 
-pylint --fail-under 9 democritus_json/*.py
+pylint --fail-under 9 d8s_json/*.py
 
-flake8 democritus_json/ tests/
+flake8 d8s_json/ tests/
 
-bandit -r democritus_json/
+bandit -r d8s_json/
 
 # we run black again at the end to undo any odd changes made by any of the linters above
-black democritus_json/ tests/
+black d8s_json/ tests/
